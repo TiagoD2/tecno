164ca1935a017c4b751a78d2593c8f6e450216ab
TiagoD2 <brfree007@gmail.com>
2024-09-03 14:14:36 -0300

    salvando

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background('red');
+  }function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background('red');
+  }
